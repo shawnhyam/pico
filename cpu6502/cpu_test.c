e672e1f687e5c303eb01eafadfe0155ab6a11efe
Shawn Hyam <shawn@amarantin.tech>
2022-06-11 23:07:59 -0400

    Got a minimal program running in Pico 6502.

--- cpu6502/cpu_test.c
+++ cpu6502/cpu_test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "pico/stdlib.h"
 // #include "vrEmu6502.h"
 
@@ -25,7 +26,19 @@ int main() {
     stdio_init_all();
     printf("Starting up...\n");
 
-    // TODO Load a ROM into the mem array...
+    // Load a ROM into the mem array...
+    // from https://skilldrick.github.io/easy6502/
+    // 0600: a9 01 8d 00 02 a9 05 8d 01 02 a9 08 8d 02 02 
+    static const uint8_t program[] = { 0xa9, 0x01, 0x8d, 0x00, 0x02, 0xa9, 0x05, 0x8d, 0x01, 0x02, 0xa9, 0x08, 0x8d, 0x02, 0x02 };
+    memcpy(mem+0x0600, program, sizeof(program));
+
+    mem[0xfffc] = 0x00;
+    mem[0xfffd] = 0x06;
+
+    // TODO generate video that matches the easy6502 specification
+    // NOTE more programs at https://github.com/r00ster91/easy6502
+    
+
 
     reset6502();
     for (int i=0; i<10; i++) {
